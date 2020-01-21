d533e6031826cefbf7c99680cb89b2b98e0500f0
Dillon Bourne <dillon_bourne82@live.com>
2020-01-21 17:18:25 -0500

    GeneralStats finished and is mergeable with PlayerInfo

--- GeneralStats.py
+++ GeneralStats.py
@@ -1,23 +1,24 @@
 import requests
 from PlayerInfo import PlayerInfo
 from copy import deepcopy
-import numpy as np 
 import pandas as pd
+import numpy as np
 
 class GeneralStats:
     
     def __init__(self,league):
         self.pi = PlayerInfo() # not permanent one class will be composed of all types of stats
         self.league = league
-        self.identity = []
-        self.goals = []
-        self.assists = []
-        self.apps = []
-        self.mins = []
-        self.yellows = []
-        self.reds = []
-        self.sub_on = []
-        self.sub_off = []
+        self.general_info = {
+            "goals": [],
+            "assists": [],
+            "apps": [],
+            "mins": [],
+            "yellows": [],
+            "reds": [],
+            "sub_on": [],
+            "sub_off": []
+        }
         self.general_urls = {
             "goals" : "https://footballapi.pulselive.com/football/stats/ranked/players/goals?page=0&pageSize=20&compSeasons=274&comps=1&compCodeForActivePlayer=" + self.league + "&altIds=true",
             "assists" : "https://footballapi.pulselive.com/football/stats/ranked/players/goal_assist?page=0&pageSize=20&compSeasons=274&comps=1&compCodeForActivePlayer="+self.league+"&altIds=true",
@@ -28,6 +29,8 @@ class GeneralStats:
             "sub_on" : "https://footballapi.pulselive.com/football/stats/ranked/players/total_sub_on?page=0&pageSize=20&compSeasons=274&comps=1&compCodeForActivePlayer="+self.league+"&altIds=true",
             "sub_off" : "https://footballapi.pulselive.com/football/stats/ranked/players/total_sub_off?page=0&pageSize=20&compSeasons=274&comps=1&compCodeForActivePlayer="+self.league+"&altIds=true"
         }
+        self.parse_stats(self.general_info.keys())
+        self.create_gs_df()
         
     def list_content_empty(self,li):
         """check if the end of json data is reached"""
@@ -56,63 +59,65 @@ class GeneralStats:
         return ls_responses
     
     def parse_stats(self,stat):
-        pages = self.get_(stat)
-        for page in pages:
-            for player in range(len(page['stats']['content'])):
-                if stat == "goals":
-                    self.goals.append(tuple((int(page['stats']['content'][player]['owner']['playerId']),int(page['stats']['content'][player]['value']))))
-                elif stat == "assists":
-                    self.assists.append(tuple((int(page['stats']['content'][player]['owner']['playerId']),int(page['stats']['content'][player]['value']))))
-                elif stat == "apps":
-                    self.apps.append(tuple((int(page['stats']['content'][player]['owner']['playerId']),int(page['stats']['content'][player]['value']))))
-                    
-    #take df.index (id) from self .pi and make a new DataFrame with that index & and extra columns all filled with NaN called self.gs
-    #wherever the id matches index, fill in the appropriate stat
+            """
+            Input: list of stat field names
+            Process: parses json data from the api and stores it in
+            the corresponding instance variables
+            Output: None
+            """
+            for s in stat:
+                pages = self.get_(s)
+                for page in pages:
+                    for player in range(len(page['stats']['content'])):
+                        self.general_info[s].append(tuple((int(page['stats']['content'][player]['owner']['playerId']),int(page['stats']['content'][player]['value']))))
+                       
+    
     def create_gs_df(self):
         list_of_ids = {
                 "goals": [],
                 "assists": [],
-                "apps": []
+                "apps": [],
+                "mins": [],
+                "yellows": [],
+                "reds": [],
+                "sub_on": [],
+                "sub_off": []
                 }
         list_of_vals = {
                 "goals": [],
                 "assists": [],
-                "apps": []
+                "apps": [],
+                "mins": [],
+                "yellows": [],
+                "reds": [],
+                "sub_on": [],
+                "sub_off": []
                 }
-        # collect ids from goals
-        for val in self.goals:
-            list_of_ids['goals'].append(val[0])
-            list_of_vals['goals'].append(val[1])
-        # collect ids from assists
-        for val in self.assists:
-            list_of_ids['assists'].append(val[0])
-            list_of_vals['assists'].append(val[1])
-        # collect ids from apps
-        for val in self.apps:
-            list_of_ids['apps'].append(val[0])
-            list_of_vals['apps'].append(val[1])
-
+        # collect ids & values from instance dictionary "general_info"
+        for s in list_of_ids.keys():
+            for val in self.general_info[s]:
+                list_of_ids[s].append(val[0])
+                list_of_vals[s].append(val[1])
+            
         # create dataframes
         goals_df = pd.DataFrame(index=list_of_ids['goals'],columns=['Goals'])
         assists_df = pd.DataFrame(index=list_of_ids['assists'],columns=['Assists'])
         apps_df = pd.DataFrame(index=list_of_ids['apps'],columns=['Appearances'])
-        # fill dataframes
-        goals_df['Goals'] = list_of_vals['goals']
-        assists_df['Assists'] = list_of_vals['assists']
-        apps_df['Appearances'] = list_of_vals['apps']
-        # join dataframes
-        self.gs = goals_df.join(assists_df,how='outer').join(apps_df,how='outer')
-            
+        mins_df = pd.DataFrame(index=list_of_ids['mins'],columns=['Minutes Played'])
+        yellows_df = pd.DataFrame(index=list_of_ids['yellows'],columns=['Yellow Cards'])
+        reds_df = pd.DataFrame(index=list_of_ids['reds'],columns=['Red Cards'])
+        sub_on_df = pd.DataFrame(index=list_of_ids['sub_on'],columns=['Subbed On'])
+        sub_off_df = pd.DataFrame(index=list_of_ids['sub_off'],columns=['Subbed Off'])
         
+        # fill dataframes
+        dfs = [goals_df,assists_df,apps_df,mins_df,yellows_df,reds_df,sub_on_df,sub_off_df]
+        for df,keys in zip(dfs,list_of_vals.keys()):
+            df[df.columns[0]] = list(map(int,list_of_vals[keys]))
+            
+        # join dataframes
+        self.gs = dfs[0].join(dfs[1:],how='outer')
+        # replace NaNs
+        self.gs.fillna(0,inplace=True)
                 
 g = GeneralStats('EN_PR')
-g.parse_stats('goals')
-g.parse_stats('apps')
-g.parse_stats('assists')
-g.create_gs_df()
 print(g.gs)
-#print(g.pi.df)
-
-#print(len(g.goals))
-#print(len(g.assists))
-#print(g.apps)
