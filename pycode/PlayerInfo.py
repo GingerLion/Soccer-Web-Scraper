4c6d1b8d84167a6437e6b88ce2bf439bb6521a7c
Dillon Bourne <dillon_bourne82@live.com>
2020-02-02 15:58:30 -0500

    changed club & player scrape strategy

--- pycode/PlayerInfo.py
+++ pycode/PlayerInfo.py
@@ -1,5 +1,5 @@
 import requests
-import bs4
+import re
 from bs4 import BeautifulSoup 
 import numpy as np
 import pandas as pd
@@ -30,7 +30,7 @@ class PlayerInfo:
         # list for relevant club names
         self.clubs = []
         # link for football api
-        self.players_url = "https://footballapi.pulselive.com/football/players?pageSize=30&compSeasons=274&altIds=true&page=0&type=player&id=-1&compSeasonId=274"
+        self.players_url = "https://footballapi.pulselive.com/football/teams/?/compseasons/274/staff?pageSize=30&compSeasons=274&altIds=true&page=0&type=player"
         # link for current premier league clubs
         self.clubs_url = "https://www.premierleague.com/clubs"
         # initialize pandas DataFrame object
@@ -45,62 +45,52 @@ class PlayerInfo:
     def get_clubs(self):
         response = requests.get(self.clubs_url)
         soup = BeautifulSoup(response.content,'html.parser')
-        for clubName in soup.find_all("h4",{"class":"clubName"}):
-            self.clubs.append(clubName.text)
-            
-    def list_content_empty(self,li):
-        """check if the end of json data is reached"""
-        if not li['content']:
-            return True
-        else:
-            return False
+        clubs_list = soup.find_all("a")
+        #extract href links because they contain club number
+        clubs_links = [link['href'] for link in clubs_list]
+        #narrow down to links that have this regex pattern
+        clubs = [link for link in clubs_links if re.search('clubs/[0-9]+',link)]
+        #tidy-up club names
+        for i in range(len(clubs)):
+            clubs[i] = clubs[i].replace('-and-','-&-')
+            clubs[i] = clubs[i].replace('-',' ')
+        #split link to get club number & club name in tuples
+        self.club_and_num = [(i.split("/")[2],i.split("/")[3]) for i in clubs]
     
     def get_player_info(self):
-        i = 0
         response = dict()
-        
-        while True:
-            if i > 0: # alter GET message to change pages
-                self.players_url = self.players_url.replace(f'page={i-1}',f'page={i}')
-                response = requests.get(self.players_url).json()
-            else:
-                response = requests.get(self.players_url).json()
-            
-            # if no more content
-            if self.list_content_empty(response):
-                break
-            
+        # for each club
+        for i in range(len(self.club_and_num)):
+            response = requests.get(self.players_url.replace(f'teams/?/',f'teams/{self.club_and_num[i][0]}/')).json()
             try: 
-                players = response['content']
+                players = response['players']
+                #for every player in the club
                 for p in players:
                     try:
-                        if p['currentTeam']['name'] in self.clubs:
-                            self.player_info['id'].append(int(p['playerId']))
-                            self.player_info['name'].append(p['name']['display'])
-                            self.player_info['role'].append(p['info']['position'])
-                            self.player_info['position'].append(p['info']['positionInfo'])
-                            try: 
-                                self.player_info['shirtNum'].append(int(p['info']['shirtNum']))
-                            except:
-                                self.player_info['shirtNum'].append(0)
-                            try:
-                                self.player_info['club'].append(p['currentTeam']['name'])
-                            except:
-                                self.player_info['club'].append('N/A')
-                            try:
-                                self.player_info['country'].append(p['birth']['country']['country'])
-                            except:
-                                self.player_info['country'].append('N/A')
-                            try:
-                                self.player_info['dob'].append(str(parse(p['birth']['date']['label']).date()))
-                            except:
-                                self.player_info['dob'].append('N/A')
+                        self.player_info['id'].append(int(p['id']))
+                        self.player_info['name'].append(p['name']['display'])
+                        self.player_info['role'].append(p['info']['position'])
+                        self.player_info['position'].append(p['info']['positionInfo'])
+                        try: 
+                            self.player_info['shirtNum'].append(int(p['info']['shirtNum']))
+                        except:
+                            self.player_info['shirtNum'].append(0)
+                        try:
+                            self.player_info['club'].append(self.club_and_num[i][1])
+                        except:
+                            self.player_info['club'].append('N/A')
+                        try:
+                            self.player_info['country'].append(p['birth']['country']['country'])
+                        except:
+                            self.player_info['country'].append('N/A')
+                        try:
+                            self.player_info['dob'].append(str(parse(p['birth']['date']['label']).date()))
+                        except:
+                            self.player_info['dob'].append('N/A')
                     except:
                         pass
             except:
                 pass
-            
-            i = i + 1
             response = dict()
             
     def convert_to_df(self):
@@ -111,5 +101,8 @@ class PlayerInfo:
         #clean some data
         self.df.replace('N/A',np.NaN,inplace=True)
         self.df['shirtNum'].replace(0,np.NaN,inplace=True)
+        #capitalize first letter of column names
+        self.df.columns = [x.title() for x in self.df.columns]
         self.df.rename(columns={ "club":"Club","country":"Country","dob":"Dob","name":"Name","position":"Position","role":"Role","shirtNum":"Shirt Number"},inplace=True)
         
+
